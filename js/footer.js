e7c8c81c63d4d9d74ef948c3feee014c8c68e9fc
Yana <yana.melnykova@nure.ua>
2024-05-09 18:49:23 +0300

    fix

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,3 @@
-// Використовуйте JavaScript, щоб завантажити і вставити вміст з файлу header.html
 fetch('../html/footer.html')
 .then(response => response.text())
 .then(data => document.getElementById('footer').innerHTML = data);
--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-// Використовуйте JavaScript, щоб завантажити і вставити вміст з файлу header.html
 fetch('../html/header.html')
 .then(response => response.text())
 .then(data => document.getElementById('header').innerHTML = data);
+
